f8bd98070e786855e6aaa6ec942fb63b31f8108e
Nehemiah I. Dacres <vivacarlie@gmail.com>
2019-01-26 21:15:10 -0600

    switching from Godot to Phaser

--- haunter-godot/player_char_behavior.gd
+++ haunter-godot/player_char_behavior.gd
@@ -7,9 +7,9 @@ extends Sprite
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-
 	position = Vector2(get_viewport().size.x/2, get_viewport().size.y/2)
 	scale = Vector2(2,2) 
+
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
